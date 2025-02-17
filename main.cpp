d5e30084f891c77f1896c35e16f569ec7f08c18e
IvanMev <imefd@bk.ru>
2025-02-17 21:28:21 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Stack.h"
+#include "windows.h"
+
+int main()
+{
+	SetConsoleOutputCP(1251);
+	SetConsoleCP(1251);
+	Person Person;
+	Person.Show();
+	Person.ShowFormal();
+
+}
